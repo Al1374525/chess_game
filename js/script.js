bd43b39ff537f7b81e43be1648da7ca3ca829c17
Al1374525 <albertayiteyadjin@gmail.com>
2023-05-20 12:57:58 -0400

    work in terms of defining and validating moves

--- js/script.js
+++ js/script.js
@@ -93,3 +93,126 @@ else{
         highlightSquare(squareId);
     }
 }
+
+// function to get piece at a given square
+function getPieceAtSquare(squareId){
+    const [file, rank] = squareId.split('-');
+    const fileIndex = file.charCodeAt(0) - 97;
+    const rankIndex = 8 - parseInt(rank);
+
+    return ChessBoard[rankIndex][fileIndex];
+
+}
+
+//function to validate if the move is valid for that piece
+function isValidMove(piece, targetSquare){
+    const [file, rank] = piece.split('-');
+    const fileIndex = file.charCodeAt(0) - 97;
+    const rankIndex = 8 - parseInt(rank);
+
+    //check if the target square is empty
+    if(!getPieceAtSquare[rankIndex][fileIndex]){
+        if(piece.color == 'white'){
+            if(rankIndex == piece.position.rank - 1 && fileIndex == piece.position.file){
+                return true;
+            }
+        } else{
+            if(rankIndex == piece.position.rank + 1 && fileIndex == piece.position.file){
+                return true;
+            }
+        }
+
+    }
+    
+    if (piece.type === 'rook') {
+        if (!isPathClearForRook(piece.position, targetSquare)) {
+          return false;
+        }
+      }
+
+      else if(piece.type === 'bishop'){
+        if(!isPathClearForBishop(piece.position, targetSquare)){
+            return false;
+        }
+      }
+
+      else if(piece.type === '`knight'){
+            if(!isPathClearForKnight(piece.position, targetSquare)){
+                return false;
+            }
+      }
+
+        else if(piece.type === 'queen'){
+            if(!isPathClearForQueen(piece.position, targetSquare)){
+                return false;
+            }
+        }
+}
+
+        function isPathClearForRook(currentPosition, targetSquare){
+            //determine the direction of the move, either horizontal or vertical
+            const isHorizontalMove = currentPosition.file !== targetSquare.file;
+
+            //get the start and the end positions for checking obstruction
+            const start = isHorizontalMove ? Math.min(currentPosition.file, targetSquare.file) : Math.min(currentPosition.rank, targetSquare.rank);
+            const end = isHorizontalMove ? Math.max(currentPosition.file, targetSquare.file) : Math.max(currentPosition.rank, targetSquare.rank);
+
+            //check for obstructions in the path
+            const min = Math.min(start, end);
+            const max = Math.max(start, end);
+            for(let i = min + 1; i < max; i++){
+                if(isHorizontalMove && ChessBoard[currentPosition.rank][i]){
+                    return false; //obstruction found  in horizontal path
+            }
+            else if(!isHorizontalMove && ChessBoard[i][currentPosition.file]){
+                return false; //obstruction found in vertical path
+            }
+
+            return true; // no obstruction found
+        }
+       
+
+
+      
+        
+           
+       
+ 
+
+
+
+
+//function to move a piece to a given square
+function movePiece(piece, targetSquare){
+    const [file, rank] = targetSquare.split('-');
+    const fileIndex = file.charCodeAt(0) - 97;
+    const rankIndex = 8 - parseInt(rank);
+
+    piece.position.rank = rankIndex;
+    piece.position.file = fileIndex;
+
+    //update the chessboard array
+    ChessBoard[rankIndex][fileIndex] = piece;
+
+    //remove the piece from its previous position
+    ChessBoard[piece.position.rank][piece.position.file] = null;
+
+    //update the UI
+    updateUI(piece, targetSquare);
+
+}
+
+//function to highlight a given square
+function highlightSquare(squareId){
+    const square = document.getElementById(squareId);
+    square.classList.add('highlight');
+
+}
+
+//function to remove highlight from a given square
+function removeHighlight(){
+    const highlightedsquares = document.querySelectorAll('.highlight');
+    highlightedsquares.forEach(square => square.classList.remove('highlight'));
+
+}
+}
